cb079b3b58abf897f3a09fabc7837081a43afde6
MarjoleinHordijk <m.a.hordijk@student.tue.nl>
2020-01-15 15:47:26 +0100

    GIS tutorial crash course

--- docs/source/02_GIS.rst
+++ docs/source/02_GIS.rst
@@ -2,14 +2,14 @@ GIS Tutorials
 ============================================
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 1
    :caption: PyQGIS Tutorials:
    
    Tutorial 1 <02_01_Tutorial1.rst>
    Tutorial 2 <02_02_Tutorial2.rst>
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 1
    :caption: GeoDjango Tutorials:
 
    Tutorial 3 <02_03_Tutorial3.rst>
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,10 @@ author = 'Marjolein Hordijk'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc'
+extensions = [
+    'sphinx.ext.autodoc',
+    'recommonmark'
+
 ]
 
 # Add any paths that contain templates here, relative to this directory.
